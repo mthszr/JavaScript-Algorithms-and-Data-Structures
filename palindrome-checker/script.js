d97f85f0c8edf572006b9ab84e597f29170f388f
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-09-05 19:34:10 -0300

    done: first project

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -1,7 +1,29 @@
-const checkButton = document.getElementById("heck-button");
+const mainBlock = document.getElementById("main-block");
+const checkBtn = document.getElementById("check-btn");
+const isPalindromeDiv = document.getElementById("is-palindrome-div")
 
 const isPalindrome = () => {
+    const phrase = document.getElementById("text-input").value.trim();
+
+    if (phrase === "") {
+        alert("Please input a value");
+        return null;
+    }
+
+    let cleanPhrase = phrase.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    let invertedPhrase = cleanPhrase.split('').reverse().join('');
+
+    return cleanPhrase === invertedPhrase 
+        ? `<strong>${phrase}</strong> is a palindrome.` 
+        : `<strong>${phrase}</strong> is not a palindrome.`;
 
 };
 
-checkButton.addEventListener("click", isPalindrome);
+const showResult = () => {
+    mainBlock.style.height = "10rem";
+    isPalindromeDiv.style.display = "block";
+
+    document.getElementById("result").innerHTML = isPalindrome();
+};
+
+checkBtn.addEventListener("click", showResult);
